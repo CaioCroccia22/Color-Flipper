98dc94847aff5d5e3877bbbb0e18f3b88b86083f
caiocroccia22 <caiocroccia22@gmail.com>
2023-09-03 09:31:01 -0300

    Novas variaveis  e substitução no evento

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Selecionar os elementos
 
-const colors = ["red", "green","blue", "#f15025" ]
-
-const changeBtn = document.querySelector("#btn-submit");
-const nameColor = document.querySelector("#name-color");
-const backColor = document.querySelector("#background");
+const colors = ["red", "green","blue", "#f15025" ];
 
 
-changeBtn.addEventListener("click", function(){
+const btn = document.querySelector("#btn");
+const color = document.querySelector("#name-color");
+
+
+btn.addEventListener("click", function(){
     console.log("Apertou o botão!!");
     backColor.style.background = 'green';
     nameColor.innerHTML = 'Green';
